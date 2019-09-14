8a2b909bdfb6f18a7d92ba4b33d86d19d516b161
chaninkou <michaelkoulove@yahoo.com>
2019-09-14 12:35:15 -0700

    Ok problem, able to find if string is palindrome while ignoring cases.

--- src/validpalindrome/CheckIfValidPalindromeFunction.java
+++ src/validpalindrome/CheckIfValidPalindromeFunction.java
@@ -2,38 +2,40 @@ package validpalindrome;
 
 public class CheckIfValidPalindromeFunction {
 	public boolean isPalindrome(String s) {
-		if (s.isEmpty()) {
+		// If its empty, it is automatically a valid palindrome
+		if(s.isEmpty()){
 			return true;
 		}
-
+		
+		// Starts from 0 and last element to move in the middle
 		int head = 0;
 		int tail = s.length() - 1;
-
+		
 		char cHead, cTail;
-
-		while (head <= tail) {
+		
+		// Only stop the loop when it gets to the middle means we compare them all
+		while(head <= tail){
 			cHead = s.charAt(head);
 			cTail = s.charAt(tail);
-
-			// If current character is not letter or numbers
-			if (!Character.isLetterOrDigit(cHead)) {
+			
+			// isLetterOrDigit is to ignore the non letter or digit by going to the next element
+			if(!Character.isLetterOrDigit(cHead)){
 				head++;
-			} else if (!Character.isLetterOrDigit(cTail)) {
+			} else if (!Character.isLetterOrDigit(cTail)){
 				tail--;
 			} else {
-				// If this return false, then we know immediately that they
-				// don't match
-				if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
+				// If they do not match, it is not a valid palindrome right away
+				if(Character.toLowerCase(cHead) != Character.toLowerCase(cTail)){
 					return false;
 				}
-
+				
+				// Move to the next element
 				head++;
 				tail--;
 			}
 		}
-
-		// If it gets here, that means they were always the same from first to
-		// last
+		
+		// If it gets here, that means its a valid palindrome
 		return true;
 	}
 
